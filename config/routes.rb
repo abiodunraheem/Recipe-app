f574de45e89a6213fa3db8dbda1a8e14bbf2dbaf
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-07 23:40:26 +0100

    Create foot controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'food/index'
+  get 'food/show'
+  get 'food/new'
+  get 'food/create'
   #devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ test/controllers/food_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class FoodControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get food_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get food_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get food_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get food_create_url
+    assert_response :success
+  end
+end
