78087078825f6a6c18dd3e37ae193bfcb08d17b8
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-08 10:23:47 +0100

    Fix linter error

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_food
   has_many :foods, through: :recipes_foods
-  
+
   validates :name, presence: true
   validates :user, presence: true
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
