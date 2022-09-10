355695678ee48118e785c38ef569b6834ffb9ffc
abiodunraheem <raheemabiodun23@gmail.com>
2022-09-10 01:06:31 +0100

    Add devise to recipe app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :inventory, dependent: :destroy
   has_many :recipe, dependent: :destroy
 
